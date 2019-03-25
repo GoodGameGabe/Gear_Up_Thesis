ea5fe0797240eb1fdb702f1352f6584b2109560c
Salvatore Frosceno <salfrosceno@gmail.com>
2019-03-25 23:24:12 +0000

    xyz

--- src/js/Layout.jsx
+++ src/js/Layout.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Store from "./store/appContext.jsx";
 
-import { Home } from "./views/Home.jsx";
-/*import { Explore } from "./Sal/views/explore.jsx";
-import { Login } from "./Sal/views/login.jsx";
+import { Home } from "./views/Sal/Home.jsx";
+import { Explore } from "./views/Sal/ExplorePage.jsx";
+import { Login } from "./views/Sal/login.jsx";
 
-import { Article } from "./Daph/views/article.jsx"; */
+import { Article } from "./views/Daph/article.jsx";
 
 import { Cart } from "./views/Gabe/Cart.jsx";
 import { Checkout } from "./views/Gabe/Checkout.jsx";
@@ -14,9 +14,9 @@ import { Create } from "./views/Gabe/Create.jsx";
 import { Bundle } from "./views/Gabe/Bundle.jsx";
 
 import { BundleList } from "./component/Gabe/BundleList.jsx";
-import { Navbar } from "./component/navbar.jsx";
-import { Card } from "./component/card.jsx";
-import { Jumbotron } from "./component/jumbotron.jsx";
+import { Navbar } from "./component/Sal/navbar.jsx";
+import { Card } from "./component/Sal/card.jsx";
+import { Jumbotron } from "./component/Sal/jumbotron.jsx";
 
 class Layout extends React.Component {
 	render() {
