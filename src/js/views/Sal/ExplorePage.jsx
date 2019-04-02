5519565a34f2d67c210502b84dc9e187fdc2df33
Daphness Fils-aime <sirdaph@gmail.com>
2019-04-02 00:50:43 +0000

    v

--- src/js/views/Sal/ExplorePage.jsx
+++ src/js/views/Sal/ExplorePage.jsx
@@ -10,6 +10,26 @@ export class ExplorePage extends React.Component {
 						id="explore-img"
 						src="https://images.pexels.com/photos/935952/pexels-photo-935952.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
 					/>
+					<img
+						id="explore-img"
+						src="https://images.pexels.com/photos/1875423/pexels-photo-1875423.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+					/>
+					<img
+						id="explore-img"
+						src="https://images.pexels.com/photos/1148955/pexels-photo-1148955.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+					/>
+					<img
+						id="explore-img"
+						src="https://images.pexels.com/photos/1722868/pexels-photo-1722868.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+					/>
+					<img
+						id="explore-img"
+						src="https://images.pexels.com/photos/724494/pexels-photo-724494.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+					/>
+					<img
+						id="explore-img"
+						src="https://images.pexels.com/photos/1797272/pexels-photo-1797272.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+					/>
 					<a href="https://www.w3schools.com/html/">
 						<h2>Fashion/Events</h2>
 					</a>
@@ -17,7 +37,27 @@ export class ExplorePage extends React.Component {
 				<div className="cat2 flip">
 					<img
 						id="explore-img"
+						src="https://images.pexels.com/photos/40564/ice-hockey-players-pass-forward-40564.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+					/>
+					<img
+						id="explore-img"
+						src="https://images.pexels.com/photos/1289118/pexels-photo-1289118.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+					/>
+					<img
+						id="explore-img"
+						src="https://images.pexels.com/photos/159594/soccer-football-player-sport-159594.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+					/>
+					<img
+						id="explore-img"
+						src="https://images.pexels.com/photos/1080884/pexels-photo-1080884.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+					/>
+					<img
+						id="explore-img"
 						src="https://images.pexels.com/photos/163439/football-american-football-quarterback-runner-163439.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+					/>
+					<img
+						id="explore-img"
+						src="https://images.pexels.com/photos/159550/baseball-foul-ball-hit-baseball-bat-159550.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
 					/>
 					<a href="https://www.w3schools.com/html/">
 						<h2>Sports</h2>
@@ -27,6 +67,18 @@ export class ExplorePage extends React.Component {
 					<img
 						id="explore-img2"
 						src="https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+					/>
+					<img
+						id="explore-img2"
+						src="https://images.pexels.com/photos/462118/pexels-photo-462118.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+					/>
+					<img
+						id="explore-img2"
+						src="https://images.pexels.com/photos/449627/pexels-photo-449627.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+					/>
+					<img
+						id="explore-img2"
+						src="https://images.pexels.com/photos/374811/pexels-photo-374811.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
 					/>
 					<a href="https://www.w3schools.com/html/">
 						<h2>Nature</h2>
